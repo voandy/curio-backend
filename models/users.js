b3b557b9ebd37f5da4ee2b01934948b1d0c75e7f
csugianto <csugianto@student.unimelb.edu.au>
2019-08-04 11:29:41 +1000

    create basic user model

---
+++ models/users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+
+    name: {
+      type: String,
+      required: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+    
+    dateJoined:{type: Date, default: Date.now},
+  }
+);
+
+const User = mongoose.model('user',userSchema);
+
+module.exports = User;
