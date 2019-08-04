bd60d52be5866c089d756bd6fb2049febaa62795
csugianto <csugianto@student.unimelb.edu.au>
2019-08-04 11:39:48 +1000

    create server.js and connect to mongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://christian:12341234@it-project-backend-bgclv.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+}
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// listen
+const port = process.env.PORT || 5001;
+app.listen(port, function(){
+    console.log("Listening on port " + port);
+});
