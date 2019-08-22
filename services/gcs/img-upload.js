5bc436ba31c532456b7689982e88aa6a76eddc01
Andy Vo <andy@voandy.com>
2019-08-22 15:27:03 +1000

    added GCS image uploade middleware

---
+++ services/gcs/img-upload.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const {Storage} = require('@google-cloud/storage');
+const fs = require('fs')
+
+const gcs = new Storage({
+  projectId: process.env.GCLOUD_PROJECT,
+  keyFilename: process.env.GCS_KEYFILE
+});
+
+const bucketName = process.env.GCS_BUCKET;
+const bucket = gcs.bucket(bucketName);
+
+function getPublicUrl(filename) {
+  return 'https://storage.googleapis.com/' + bucketName + '/' + filename;
+}
+
+let ImgUpload = {};
+
+ImgUpload.uploadToGcs = (req, res, next) => {
+  if(!req.file) return next();
+
+  const gcsname = req.file.originalname;
+  const file = bucket.file(gcsname);
+
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype
+    }
+  });
+
+  stream.on('error', (err) => {
+    req.file.cloudStorageError = err;
+    next(err);
+  });
+
+  stream.on('finish', () => {
+    req.file.cloudStorageObject = gcsname;
+    req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
+    next();
+  });
+
+  stream.end(req.file.buffer);
+}
+
+module.exports = ImgUpload;
